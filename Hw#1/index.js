79dd46532aedaaad5a6bb4c47b887b07e4ca28b2
Andrii-Rudys <rudys.andrii@gmail.com>
2020-02-03 18:15:27 +0200

    added homework to github

--- Hw#1/index.js
+++ Hw#1/index.js
@@ -4,27 +4,21 @@ let sneakers = 90.2345;
 
 //1.
 let maxPrice = Math.min(gloves,backpack, sneakers);
-console.log(maxPrice);
 
 //2.
 let minPrice = Math.max(gloves,backpack, sneakers);
-console.log(minPrice);
 
 //3.
 let totalPrice = (gloves+backpack+sneakers);
-console.log (totalPrice);
 
 //4.
 let totalRoundedPrice = Math.floor(gloves)+Math.floor(backpack)+ Math.floor(sneakers);
-console.log(totalRoundedPrice);
 
 //5.
 let hundredPrice = Math.round(totalPrice/100)*100;
-console.log(hundredPrice);
 
 //6.
 let isOdd = totalRoundedPrice % 2 === 1;
-console.log(isOdd);
 
 //7.
 let cash = 500;
@@ -33,15 +27,12 @@ console.log(rest);
 
 //8.
 let PriceAverage = (totalPrice/3) .toFixed(2);
-console.log (PriceAverage);
 
 //9.
 let randomDiscount = Math.random()*100;
 let priceWithDiscount = (totalPrice - (totalPrice/100) * randomDiscount) .toFixed(2);
-console.log(priceWithDiscount);
 let discount = (totalPrice/100) * randomDiscount;
 let netIncome =  (totalPrice/2 - discount).toFixed(2);
-console.log(netIncome);
 
 //Advanced
 let templateString = `Max price: ${maxPrice}
